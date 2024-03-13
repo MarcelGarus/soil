7e1cf5b468b8ae09d38b181fbad9d44865b88939
Marcel Garus <github@marcelgarus.dev>
2024-03-14 00:15:33 +0100

    Fix VM

--- soil.c
+++ soil.c
@@ -10,32 +10,52 @@ void panic(int exit_code, char* msg) {
 typedef uint8_t Byte;
 typedef uint64_t Word;
 
-typedef struct Vm {
-  Word reg[16]; // ip, sp, st, a, b, c, d, e
-  Byte* mem;
-} Vm;
+Word reg[16]; // ip, sp, st, a, b, c, d, e
+Byte* mem;
 
-Vm create_vm() {  // int image_len, char* image
-  Vm vm;
-  for (int i = 0; i < 8; i++) vm.reg[i] = 0;
-  vm.mem = malloc(1000);
-  return vm;
+Word read_word(Byte* bin, Word pos) {
+  Word word;
+  for (int i = 7; i >= 0; i--)
+    word = (word << 8) + bin[pos + i];
+  return word;
 }
 
-void dump_reg(Vm vm) {
+void init_vm(Byte* bin, int len) {
+  for (int i = 0; i < 8; i++) reg[i] = 0;
+  mem = malloc(1000);
+
+  if (bin[0] != 's' || bin[1] != 'o' || bin[2] != 'i' || bin[3] != 'l')
+    panic(1, "Magic bytes don't match.");
+  int num_devices = bin[4];
+  printf("num devices %d\n", num_devices);
+  int cursor = 5;
+  for (int i = 0; i < num_devices; i++) {
+    int type = bin[cursor];
+    int pointer = read_word(bin, cursor + 1);
+    cursor += 9;
+    if (type == 3) {
+      // machine code
+      int len = read_word(bin, pointer);
+      int machine_code_start = pointer + 8;
+      printf("machine code is at %x\n", machine_code_start);
+      for (int j = 0; j < len; j++) mem[j] = bin[machine_code_start + j];
+    }
+  }
+}
+
+void dump_reg() {
   printf(
-      "ip = %ld, sp = %ld, st = %ld, a = %ld, b = %ld, c = %ld, d = %ld, e = "
-      "%ld\n",
-      vm.reg[0], vm.reg[1], vm.reg[2], vm.reg[3], vm.reg[4], vm.reg[5],
-      vm.reg[6], vm.reg[7]);
+    "ip = %ld, sp = %ld, st = %ld, a = %ld, b = %ld, c = %ld, d = %ld, e = "
+    "%ld\n", reg[0], reg[1], reg[2], reg[3], reg[4], reg[5], reg[6], reg[7]);
 }
 
 typedef Byte Reg; // 4 bits would actually be enough, but meh
 
-void run_single(Vm* vm) {
-  Word* reg = vm->reg;
-  Byte* mem = vm->mem;
+void devicecall(int device) {
+  panic(1, "TODO: devicecall\n");
+}
 
+void run_single() {
   #define IP reg[0]
   #define SP reg[1]
   #define ST reg[2]
@@ -43,7 +63,6 @@ void run_single(Vm* vm) {
   #define REG2 reg[mem[IP + 1] >> 4]
 
   Byte opcode = mem[IP];
-  printf("opcode = %x\n", opcode);
   switch (opcode) {
     case 0x00: IP += 1; break; // nop
     case 0xe0: printf("Panicked\n"); return; // panic
@@ -60,7 +79,7 @@ void run_single(Vm* vm) {
     case 0xf1: if (ST != 0) IP = *(Word*)(mem + IP + 1); break; // cjump
     case 0xf2: SP -= 8; *(Word*)(mem + SP) = REG1; IP = *(Word*)(mem + IP + 1); break; // call
     case 0xf3: IP = *(Word*)(mem + SP); SP += 8; break; // ret
-    case 0xf4: printf("TODO: syscall\n"); return; // syscall
+    case 0xf4: Byte device = mem[IP + 1]; devicecall(device); IP += 2; break; // devicecall
     case 0xc0: ST = REG1 - REG2; IP += 2; break; // cmp
     case 0xc1: ST = ST == 0 ? 1 : 0; IP += 1; break; // isequal
     case 0xc2: ST = (int64_t)ST < 0 ? 1 : 0; IP += 1; break; // isless
@@ -79,53 +98,30 @@ void run_single(Vm* vm) {
   }
 }
 
-void test_fib_vm() {
-  Vm vm = create_vm();
-  Byte code[] = {
-      0xd2, 0b00000011, 0, // moveib a 0
-      0xd2, 0b00000100, 1, // moveib b 1
-      0xd2, 0b00000101, 0, // moveib c 0
-      0xd0, 0b01000101,    // move c b
-      0xa0, 0b00110100,    // add b a
-      0xd0, 0b01010011,    // move a c
-      0xf0, 9,             // jump 0
-  };
-  size_t code_len = sizeof(code) / sizeof(Byte);
-
-  for (int i = 0; i < code_len; i++)
-    vm.mem[i] = code[i];
-
+void run() {
   for (int i = 0; i < 100; i++) {
-    dump_reg(vm);
-    run_single(&vm);
+    dump_reg();
+    run_single();
   }
 }
 
 int main(int argc, char** argv) {
-  test_fib_vm();
-  exit(0);
-  
-  if (argc < 2) panic(1, "Expected file to run.");
-  FILE* file = fopen(argv[1], "r");
-
   size_t cap = 8;
   size_t len = 0;
-  char *image = (char*) malloc(8);
-  if (image == NULL) panic(2, "Out of memory.");
+  char *bin = (char*) malloc(8);
+  if (bin == NULL) panic(2, "Out of memory.");
   char ch;
-
   do {
-    ch = fgetc(file);
+    ch = fgetc(stdin);
     if (len == cap) {
       cap *= 2;
-      image = realloc(image, cap);
-      if (image == NULL) panic(2, "Out of memory.");
+      bin = realloc(bin, cap);
+      if (bin == NULL) panic(2, "Out of memory.");
     }
-    image[len] = ch;
+    bin[len] = ch;
     len++;
   } while (ch != EOF);
 
-  fclose(file);
-
-  Vm vm = create_vm();
+  init_vm(bin, len);
+  run();
 }
