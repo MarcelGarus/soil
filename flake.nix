d5e159caf86a1ad829bc1da2b1f7ed7b7c8a33f6
Jonas Wanke <contact@jonas-wanke.com>
2024-05-20 09:44:09 +0200

    Create Flutter project

--- flake.nix
+++ flake.nix
@@ -1,15 +1,21 @@
 {
   inputs = {
     nixpkgs.url =
-      "github:nixos/nixpkgs?ref=8eb7faf85cc3d45fdca33b54bb10a2728eda899b";
-    # https://github.com/NixOS/nixpkgs/pull/311568
+      "github:nixos/nixpkgs?ref=a22a985f13d58b2bafb4964dd2bdf6376106a2d2";
+    # https://github.com/NixOS/nixpkgs/pull/311815
     flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = import nixpkgs { inherit system; };
+      let
+        pkgs = import nixpkgs { inherit system; };
+        flutter = pkgs.flutterPackages.v3_22;
       in {
-        devShell = with pkgs; mkShell { buildInputs = [ dart fasm gnumake ]; };
+        devShell = with pkgs;
+          mkShell {
+            FLUTTER_ROOT = flutter;
+            buildInputs = [ fasm flutter gnumake ];
+          };
       });
 }
